2cba57b37e8dc2d9d62298ef7f6470e46263cb4b
DavidIncarbone <david.incarbone@gmail.com>
2025-01-09 09:29:01 +0100

    fixed tags

--- models/tags.js
+++ models/tags.js
@@ -9,7 +9,5 @@ for (let i = 0; i < myTags.length; i++) {
     for (let j = 0; j < myTags[i].length; j++) {
         filteredTags.indexOf(myTags[i][j]) === -1 && filteredTags.push(myTags[i][j])
     }
-
 }
-
 export { filteredTags }
